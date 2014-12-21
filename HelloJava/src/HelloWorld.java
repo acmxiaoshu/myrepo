cf61bb8d4d7c31eb0de57559c4be0c2228785937
acmxiaoshu <acmxiaoshu@163.com>
2014-12-21 20:09:40 +0800

    Add a line.

--- HelloJava/src/HelloWorld.java
+++ HelloJava/src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 	public static void main(){
 		System.out.println("Hello World~");
 		System.out.println("Hello World~");
-		System.out.println("Hello World~sdfasdfsdf");
+		
 		System.out.println("Hello 11111f");
 		
 		
